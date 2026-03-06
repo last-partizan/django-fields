5b3e0765b37890df64b044e9cec6b32920fe595e
Serhii Tereshchenko <serg.partizan@gmail.com>
2026-03-06 14:49:20 +0200

    chore: Hotifx

--- src/django_fields/__init__.py
+++ src/django_fields/__init__.py
@@ -1,9 +1,9 @@
-from importlib.metadata import version
+from importlib.metadata import version, PackageNotFoundError
 import os.path
 
 try:
     __version__ = version("django-fields")
-except importlib.metadata.PackageNotFoundError:
+except PackageNotFoundError:
     __version__ = 'Please install this project with a supported installer'
 
 VERSION = __version__
